06b5cee841f77543e9802cc9e76a266513391ce0
Nancy <nancycantusal@gmail.com>
2019-02-24 20:12:47 -0500

    ensured conflict resolution left what I wanted

---
+++ script.js
@@ -0,0 +1,4 @@
+//first define the audio element
+const audioString = "http://www.mariomayhem.com/downloads/sounds/super_mario_bros/smb_world_clear.wav";
+const alarm = document.createElement('audio');
+alarm.setAttribute("src", audioString);
